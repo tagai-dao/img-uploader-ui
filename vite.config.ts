182da800f62a016a9d453a404d7dc326d310f47c
terry <terry3t@gmail.com>
2026-03-20 14:46:30 +0800

    feat: set custom domain uploadimg.tagai.fun, reset vite base to /

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/img-uploader-ui/',
+  base: '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
